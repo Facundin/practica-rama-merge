b13d9def9267df2b7369a63258384b471d45eebc
Cufa <juarezfacundo39@gmail.com>
2024-12-21 16:58:41 -0300

    solucion de pedro

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function calcular(){
     document.getElementById('pantalla').value = resultado 
 }
 function deshacer(){
-    const valorPantalla = document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
+    const valorPantalla = document.getElementById('pantalla').value 
+    const resultado= valorPantalla.substring(0,valorPantalla.length -1)
+    document.getElementById('pantalla').value = resultado; 
 }
