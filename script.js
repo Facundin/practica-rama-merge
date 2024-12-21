c472887d443dfa7dd10ee541944fd3b84f17ee8a
Cufa <juarezfacundo39@gmail.com>
2024-12-21 16:46:02 -0300

    soy la crab y lo solucione

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ function calcular(){
     const resultado = eval(valorPantalla)
     document.getElementById('pantalla').value = resultado 
 }
+function deshacer(){
+    const valorPantalla = document.getElementById('pantalla').value;
+    document.getElementById('pantalla').value = valorPantalla.slice(0,-1);
+}
